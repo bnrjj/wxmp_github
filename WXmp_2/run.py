36ea9a9745065ddbfd8a119f15e9e415a42c9109
bnrjj <huangjjbnr@126.com>
2018-12-17 11:52:05 +0800

    this is realmain

--- WXmp_2/run.py
+++ WXmp_2/run.py
@@ -3,17 +3,23 @@
 
 from wxpy import *
 from wxmpdata import print_mes
+import time,random
 
 def mpupdate(mp,bot):
     lenmp = len(mp)
     print lenmp
-
     @bot.register(chats=mp, except_self=False)
     def waibufun(ms):
         print_mes(ms)
         #print len(mpup)
         #print ms.raw
         #return  len(mpup)
+        #bf = bot.friends().search('ouy')[0]
+	bf = bot.file_helper
+	#rt = int(time.time())
+	r = random.randint(1,10)
+	if r == 7:
+	    bf.send('hello!')
         mpup = bot.mps(update=True)
         print len(mpup)
         if lenmp != len(mpup):
